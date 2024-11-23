5c0f3bfcf1c2d5da844c8900ddd8a06fde9c5e02
lakshman-123 <85126140+lakshman-123@users.noreply.github.com>
2024-11-23 20:01:01 +0100

    Update serverStop.sh

--- scripts/serverStop.sh
+++ scripts/serverStop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo chmod u+x /home/ec2-user/apache-tomcat-9.0.70/bin/shutdown.sh
-sudo /home/ec2-user/apache-tomcat-9.0.70/bin/shutdown.sh
+sudo chmod u+x /home/ec2-user/apache-tomcat-9.0.97/bin/shutdown.sh
+sudo /home/ec2-user/apache-tomcat-9.0.97/bin/shutdown.sh
